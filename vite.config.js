db36f8e084b07ad07c3becefd85733f8fcfc957c
00IMPERA <nelutzyou@gmail.com>
2026-04-16 07:24:37 +0200

    Enhance Vite configuration with build options

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    target: "esnext",
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "node:buffer": "buffer",
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
 });
+ 
